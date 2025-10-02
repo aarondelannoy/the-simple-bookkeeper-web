e9a72198e5204939fa888d736505aac8d57ebe3b
aarondelannoy <aaron.delannoy@gmail.com>
2025-10-02 14:28:09 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/the-simple-bookkeeper-web/', 
   plugins: [
     react(),
     mode === 'development' &&
